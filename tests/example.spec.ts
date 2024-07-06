b5f2e4135d6a206c071ed0305d779fd85d1af761
Dmytro Lavryk <62079347+LavrykD@users.noreply.github.com>
2024-07-06 13:01:18 +0300

    first commit

---
+++ tests/example.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await test.step("Open page", async() => {
+    await page.goto('https://playwright.dev/');
+  })
+  
+
+  // Expect a title "to contain" a substring.
+  await expect(page, "Check page title").toHaveTitle(/Playwright/);
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible(); 
+});
